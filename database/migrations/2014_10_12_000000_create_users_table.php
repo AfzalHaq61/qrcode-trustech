ec351c7da2a51a593ffca92fd30fc7970eb4c032
Nooramin1 <31203917+Nooramin1@users.noreply.github.com>
2023-06-08 14:48:56 +0500

    push work

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,31 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->bigInteger('role_id')->default(2);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('auth_type')->nullable();
+            $table->string('choosed_theme')->default('light');
+            $table->longText('profile_image')->nullable();
+            $table->string('plan_id')->nullable();
+            $table->integer('term')->nullable();
+            $table->longText('plan_details')->nullable();
+            $table->string('plan_validity')->nullable();
+            $table->timestamp('plan_activation_date')->nullable();
+            $table->string('billing_name')->nullable();
+            $table->string('type')->nullable();
+            $table->string('vat_number')->nullable();
+            $table->longText('billing_address')->nullable();
+            $table->string('billing_city')->nullable();
+            $table->string('billing_state')->nullable();
+            $table->string('billing_zipcode')->nullable();
+            $table->string('billing_country')->nullable();
+            $table->string('billing_phone')->nullable();
+            $table->string('billing_email')->nullable();
+            $table->integer('status')->default(1);
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
         });
     }
 
--- resources/js/Layouts/Partials/Sidebar.vue
+++ resources/js/Layouts/Partials/Sidebar.vue
@@ -10,7 +10,7 @@
 
           <hr class="h-px mt-0 bg-transparent bg-gradient-to-r from-transparent via-black/40 to-transparent" />
 
-          <div class="items-center block w-auto max-h-screen overflow-auto h-sidenav grow basis-full">
+          <div class="items-center block w-auto max-h-screen overflow-auto h-sidenav grow basis-full" style="height:100%">
             <ul class="flex flex-col pl-0 mb-0">
               <li class="mt-0.5 w-full">
                 <a class="py-2.7 shadow-soft-xl text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap rounded-lg bg-white px-4 font-semibold text-slate-700 transition-colors" href="./pages/dashboard.html">
@@ -205,19 +205,10 @@
             <p class="invisible hidden text-gray-800 text-red-500 text-red-600 after:bg-gradient-to-tl after:from-gray-900 after:to-slate-800 after:from-blue-600 after:to-cyan-400 after:from-red-500 after:to-yellow-400 after:from-green-600 after:to-lime-400 after:from-red-600 after:to-rose-400 after:from-slate-600 after:to-slate-300 text-lime-500 text-cyan-500 text-slate-400 text-fuchsia-500"></p>
             <div class="after:opacity-65 after:bg-gradient-to-tl after:from-slate-600 after:to-slate-300 relative flex min-w-0 flex-col items-center break-words rounded-2xl border-0 border-solid border-blue-900 bg-white bg-clip-border shadow-none after:absolute after:top-0 after:bottom-0 after:left-0 after:z-10 after:block after:h-full after:w-full after:rounded-2xl after:content-['']" sidenav-card>
               <div class="mb-7.5 absolute h-full w-full rounded-2xl bg-cover bg-center" style="background-image: url('./assets/img/curved-images/white-curved.jpeg')"></div>
-              <div class="relative z-20 flex-auto w-full p-4 text-left text-white">
-                <div class="flex items-center justify-center w-8 h-8 mb-4 text-center bg-white bg-center rounded-lg icon shadow-soft-2xl">
-                  <i class="top-0 z-10 text-lg leading-none text-transparent ni ni-diamond bg-gradient-to-tl from-slate-600 to-slate-300 bg-clip-text opacity-80" sidenav-card-icon></i>
-                </div>
-                <div class="transition-all duration-200 ease-nav-brand">
-                  <h6 class="mb-0 text-white">Need help?</h6>
-                  <p class="mt-0 mb-4 text-xs font-semibold leading-tight">Please check our docs</p>
-                  <a href="https://www.creative-tim.com/learning-lab/tailwind/html/quick-start/soft-ui-dashboard/" target="_blank" class="inline-block w-full px-8 py-2 mb-0 text-xs font-bold text-center text-black uppercase transition-all ease-in bg-white border-0 border-white rounded-lg shadow-soft-md bg-150 leading-pro hover:shadow-soft-2xl hover:scale-102">Documentation</a>
-                </div>
-              </div>
+          
             </div>
             <!-- pro btn  -->
-            <a class="inline-block w-full px-6 py-3 my-4 text-xs font-bold text-center text-white uppercase align-middle transition-all ease-in border-0 rounded-lg select-none shadow-soft-md bg-150 bg-x-25 leading-pro bg-gradient-to-tl from-purple-700 to-pink-500 hover:shadow-soft-2xl hover:scale-102" target="_blank" href="https://www.creative-tim.com/product/soft-ui-dashboard-pro-tailwind?ref=sidebarfree">Upgrade to pro</a>
+           
           </div>
         </aside>
 </template>
