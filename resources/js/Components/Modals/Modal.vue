59cc409878295da93beca9212ca21e5979389624
Nooramin1 <31203917+Nooramin1@users.noreply.github.com>
2023-07-03 16:05:20 +0500

    push work

--- resources/js/Components/Modals/Modal.vue
+++ resources/js/Components/Modals/Modal.vue
@@ -1,61 +1,54 @@
-<script>
-import { ref } from 'vue';
-
-export default {
-    props: {
-        Buttontext: {
-            type: String,
-            required: true,
-        },
-    },
-    setup() {
-        const showModal = ref(false);
-
-        const openModal = () => {
-            showModal.value = true;
-        };
-
-        const closeModal = () => {
-            showModal.value = false;
-        };
-
-        const proceed = () => {
-            // Perform the desired action when the user clicks "Yes, proceed"
-            console.log('Proceed with deletion');
-        };
-
-        return {
-            showModal,
-            openModal,
-            closeModal,
-            proceed,
-        };
-    },
-};
-</script>
-
 <template>
-    <div>
-        <!-- Button to open the modal -->
-        <button @click="openModal" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
-            {{ Buttontext }}
-        </button>
+    <TransitionRoot as="template" :show="open">
+    <Dialog as="div" class="relative z-10" @close="open = false">
+      <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
+        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+      </TransitionChild>
 
-        <!-- Modal component -->
-        <div v-if="showModal" class="fixed inset-0 flex items-center justify-center z-50">
-            <div class="bg-white rounded-lg shadow-lg p-6">
-                <div class="text-danger text-lg font-bold mb-4">WARNING!</div>
-                <div class="mb-4">This action will remove user account and user data. It is not reversible.</div>
-                <div class="flex justify-end">
-                    <button @click="closeModal"
-                        class="bg-gray-400 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded mr-2">
-                        Cancel
-                    </button>
-                    <button class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded" @click="proceed">
-                        Yes, proceed
-                    </button>
+      <div class="fixed inset-0 z-10 overflow-y-auto">
+        <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+            <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+              <div class="sm:flex sm:items-start">
+                <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-alert-triangle" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="#ea0606" fill="none" stroke-linecap="round" stroke-linejoin="round">
+   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+   <path d="M10.24 3.957l-8.422 14.06a1.989 1.989 0 0 0 1.7 2.983h16.845a1.989 1.989 0 0 0 1.7 -2.983l-8.423 -14.06a1.989 1.989 0 0 0 -3.4 0z"></path>
+   <path d="M12 9v4"></path>
+   <path d="M12 17h.01"></path>
+</svg>
                 </div>
-            </div>
+                <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                  <DialogTitle as="h3" class="text-base font-semibold leading-6 text-gray-900">{{props.modalData.title}}</DialogTitle>
+                  <div class="mt-2">
+                    <p class="text-sm text-gray-500">{{props.modalData.desc}}</p>
+                  </div>
+                </div>
+              </div>
+              <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+                <Link :href="props.modalData.link" type="button" class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto" @click="open = false">{{ props.modalData.btnText }}</Link>
+                <button type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto" @click="open = false" ref="cancelButtonRef">Cancel</button>
+              </div>
+            </DialogPanel>
+          </TransitionChild>
         </div>
-    </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
 </template>
+<script setup>
+    import { ref,defineExpose} from 'vue'
+    import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue'
+    const open = ref(false)
+    const props = defineProps({
+        modalData: Object,
+          
+    });
+    function childMethod() {
+        open.value=true;
+    }    
+    defineExpose({
+            childMethod
+        });
+        
+</script>
